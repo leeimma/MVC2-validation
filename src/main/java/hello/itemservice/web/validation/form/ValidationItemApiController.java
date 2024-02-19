a5741c49f2bffbf110db1d42ef1b9cdf8e32e7b3
imina <leeimma@naver.com>
2024-02-19 22:50:48 +0900

    섹션 5. 검증2 - Bean Validation (V4)

---
+++ src/main/java/hello/itemservice/web/validation/form/ValidationItemApiController.java
@@ -0,0 +1,30 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+
+        log.info("API");
+
+        if (bindingResult.hasErrors()) {
+            log.info("{}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("");
+        return form;
+    }
+
+}
